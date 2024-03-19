52872804f0fad57634bdb1c9acda8301eb858341
alapandas3 <alapan.das@geotechinfo.net>
2024-03-19 17:08:16 +0530

    Changed the QR Code Size

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
 window.addEventListener("DOMContentLoaded", (event) => {
-    
+
 
     let inputtext = document.getElementById("ip");
     let inptext;
     let imgc = document.getElementById('qrc')
-    let colors=["#6096BA","#a3cef1","#8b8c89","#e7ecef",];
+    let colors = ["#6096BA", "#a3cef1", "#8b8c89", "#e7ecef",];
     function getImage() {
-        var url = " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
+        var url = " https://api.qrserver.com/v1/create-qr-code/?size=250x250&data="
         inptext = inputtext.value
         const finalurl = url.concat(inptext);
         if (inptext.length <= 0) {
@@ -16,15 +16,18 @@ window.addEventListener("DOMContentLoaded", (event) => {
         }
         imgc.style.display = 'block';
         imgc.src = finalurl;
-        imgc.onload=function(){document.body.style.backgroundColor = colors[Math.floor(Math.random()*10)];}
+        // imgc.onload = function () {
+            document.body.style.backgroundColor = colors[0]; 
+            // colors[Math.floor(Math.random() * 10)];
+        // }
     }
     const button = document.getElementById("button")
     button.addEventListener('click', function () {
         getImage();
         // animateBg(0);
     })
-    
-    
+
+
 
 
 });
