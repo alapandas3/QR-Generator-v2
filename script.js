8319d9246953a6fefb329020064d13713fc287cc
alapandas3 <alapan.das@geotechinfo.net>
2024-03-19 17:04:05 +0530

    Modified Scipt

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 
 window.addEventListener("DOMContentLoaded", (event) => {
+    
 
     let inputtext = document.getElementById("ip");
     let inptext;
     let imgc = document.getElementById('qrc')
-
+    let colors=["#6096BA","#a3cef1","#8b8c89","#e7ecef",];
     function getImage() {
         var url = " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
         inptext = inputtext.value
@@ -15,14 +16,15 @@ window.addEventListener("DOMContentLoaded", (event) => {
         }
         imgc.style.display = 'block';
         imgc.src = finalurl;
-        document.body.style.backgroundColor = "#6096BA";
+        imgc.onload=function(){document.body.style.backgroundColor = colors[Math.floor(Math.random()*10)];}
     }
     const button = document.getElementById("button")
     button.addEventListener('click', function () {
         getImage();
+        // animateBg(0);
     })
-
-
+    
+    
 
 
 });
