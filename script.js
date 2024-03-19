6ee172928d173f113dce7e2f32833a4ad6d7c0e2
alapandas3 <alapan.das@geotechinfo.net>
2024-03-19 16:18:46 +0530

    Modified the Script and Adjusted the Style

--- script.js
+++ script.js
@@ -3,33 +3,20 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
     let inputtext = document.getElementById("ip");
     let inptext;
-    let imgc=document.getElementById('qrc')
+    let imgc = document.getElementById('qrc')
 
     function getImage() {
         var url = " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
-        inptext=inputtext.value
-        const finalurl=url.concat(inptext);
-        imgc.style.display='block';
-        // fetch(finalurl)
-        // .then(response =>
-        //     {
-        //         if(response.ok) {
-        //             console.log("Fetched");
-        //         }else{
-        //             console.log("ERROR");
-        //         }
-        //     })
-        //     .then(data=>{
-        //         setImage(data);
-        //     })
-        //     .catch(error=>{
-        //         console.log(error);
-        //     });
-        imgc.src=finalurl;
+        inptext = inputtext.value
+        const finalurl = url.concat(inptext);
+        if (inptext.length <= 0) {
+            alert("EMPTY INPUT");
+            return
+        }
+        imgc.style.display = 'block';
+        imgc.src = finalurl;
+        document.body.style.backgroundColor = "#6096BA";
     }
-    // function setImage() {
-    //     console.log("Working :)")
-    // }
     const button = document.getElementById("button")
     button.addEventListener('click', function () {
         getImage();
