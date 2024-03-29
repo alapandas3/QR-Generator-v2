6ed184eeb941ce68dc86a4389b12c8f098dfda50
alapandas3 <alapan.das@geotechinfo.net>
2024-03-29 15:45:17 +0530

    Minor Fixes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function loader(text) {
 
     document.getElementById("loader").style.display = "block";
+    document.getElementById("one").style.display = "none";
 
     const url = `https://api.qrserver.com/v1/create-qr-code/?data=${text}&size=300x300&amp&color=fff&bgcolor=2d81ff`;
     let img = document.getElementById("one");
@@ -10,15 +11,14 @@ function loader(text) {
         .then(response => response.blob())
         .then(blob => {
             img.src = URL.createObjectURL(blob);
-            title.textContent = `Hey ${text} your Quote of the Day is:`;
+            title.textContent = `Hey ${text}`;
 
 
-            document.getElementById("loader").style.display = "flex";
-            document.getElementById("loader").style.justifyContent = "center";
-            document.getElementById("loader").style.alignItems = "center";
+
             document.getElementById("loader").innerHTML = "Loading....";
             document.getElementById("loader").style.display = "none";
             document.getElementById("one").style.display = "block";
+            document.getElementById("button").style.backgroundColor = '#0066ff';
         });
 }
 
@@ -29,6 +29,8 @@ function worker() {
 
     document.getElementById("input").disabled = true;
     document.getElementById("button").disabled = true;
+    document.getElementById("button").style.backgroundColor = 'gray';
+
     if (textinp != '') {
         fetch(url)
             .then(response => response.text())
@@ -42,9 +44,8 @@ function worker() {
         loader(textinp);
         document.getElementById("input").disabled = false;
         document.getElementById("button").disabled = false;
-
     }
     else {
-        quote.innerHTML = '';
+        quote.innerHTML = 'Invalid Input';
     }
 }
